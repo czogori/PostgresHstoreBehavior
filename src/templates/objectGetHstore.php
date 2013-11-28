478e9f5163f8b2ee183bb19895ac81ebaa90e023
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-11-28 23:50:43 +0100

    Fixed  objectGetHstore template

--- src/templates/objectGetHstore.php
+++ src/templates/objectGetHstore.php
@@ -10,7 +10,7 @@
         if (empty($key) && $this-><?php echo $columnName ?>AsArray && is_array($this-><?php echo $columnName ?>AsArray)) {
             return $this-><?php echo $columnName ?>AsArray;
         } else {
-            $hstore = str_replace('"', '', $this-><?php echo $columnName ?>AsArray);
+            $hstore = str_replace('"', '', $this-><?php echo $columnNameUnderscore ?>);
             $pairs = explode(',', $hstore);
             $array = array();
             foreach ($pairs as $pair) {
@@ -19,7 +19,7 @@
             }
 
             $this-><?php echo $columnName ?>AsArray = $array;
-            if (!empty($key) && !array_key_exists($key, $out)) {
+            if (!empty($key) && !array_key_exists($key, $array)) {
                 throw new Exception(sprintf("The key %s doesn't exist.", $key));
             }
 
