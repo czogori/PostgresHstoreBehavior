05ea06e7be7562fddf9645e38c6a369b7fbf1500
Arek Jaskólski <arek.jaskolski@gmail.com>
2014-04-17 16:12:16 +0200

    When new value is setting we can merge or overwrite with old value

--- src/templates/objectSetHstore.php
+++ src/templates/objectSetHstore.php
@@ -3,15 +3,15 @@
     /**
      * Set the value of [<?php echo $columnName ?>] column.
      *
-     * @param  array $array new value
-     * @return The   current object (for fluent API support)
+     * @param  array   $array New key-value elements.
+     * @param  boolean $merge If true new key-value elements is merging with old array otherwise old value is overwrite.
+     *
+     * @return The current object (for fluent API support)
      */
-    public function set<?php echo ucfirst($columnName) ?>(array $array = array())
+    public function set<?php echo ucfirst($columnName) ?>(array $array = array(), $merge = true)
     {
-
         if (is_array($array) && count($array) > 0) {
-
-            $this-><?php echo $columnName ?>AsArray = array_merge((array) $this-><?php echo $columnName ?>AsArray, $array);
+            $this-><?php echo $columnName ?>AsArray = $merge ? array_merge((array) $this-><?php echo $columnName ?>AsArray, $array) : $array;
             $this-><?php echo $columnNameUnderscore ?> = $this->getHstoreFormat($this-><?php echo $columnName ?>AsArray);
             $this->modifiedColumns[] = <?php echo ucfirst($tableName) ?>Peer::<?php echo strtoupper($columnNameUnderscore) ?>;
         }
